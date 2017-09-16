6bdd47a05b0c57c03c1cca83e3cda25db4a903d6
Dante Haskell Elrik <dante@elrikgroup.com>
2017-09-16 05:22:42 -0400

    Fixes git history

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,14 @@
 module Main where
 
+import Types
+import Common
 import qualified Interpreter.Read   (run)
 import qualified Interpreter.Pretty (run)
+import qualified Interpreter.Execute(run)
 
 main :: IO ()
 main = do
   (_,program) <- Interpreter.Read.run
   putStrLn (Interpreter.Pretty.run program)
+  evalStateT (Interpreter.Execute.run program) defaultState
   return ()
