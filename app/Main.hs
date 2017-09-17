4afa5d9e9c825367d8f42d3d24f40f3f9da33258
Dante Haskell Elrik <dante@elrikgroup.com>
2017-09-17 04:56:13 -0400

    CLEANUP

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ module Main where -- http://lpaste.net/3465999502194769920
 import Types -- http://lpaste.net/3752197452577374208 Types
 import Common -- http://lpaste.net/3029320831361613824 Common
 import qualified Interpreter.Read   (run)   -- http://lpaste.net/7583342056232189952
-import qualified Interpreter.Pretty (run)   -- http://lpaste.net/edit/6717294233507594240
+import qualified Interpreter.Pretty (run)   -- http://lpaste.net/6717294233507594240
 import qualified Interpreter.Execute(run)   -- http://lpaste.net/5169620089997099008
 import qualified Interpreter.Execute2(run)  -- http://lpaste.net/1413778628252008448
 
@@ -17,4 +17,3 @@ main = do
   putStrLn (Interpreter.Pretty.run program)
   evalStateT (Interpreter.Execute2.run program) defaultState2
   evalStateT (Interpreter.Execute.run program) defaultState
-  return ()
