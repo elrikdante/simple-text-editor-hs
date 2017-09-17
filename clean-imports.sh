ba9f42cc50b4d9a980253ecf46035a93c130dd9b
Dante Haskell Elrik <dante@elrikgroup.com>
2017-09-16 21:19:15 -0400

    DITTO

--- clean-imports.sh
+++ clean-imports.sh
@@ -4,12 +4,19 @@ IMPORT=${IMPORT:-Common}
 URL=${URL:-TODO: ADD A UNIVERSAL RECORD LOCATOR FOR}
 IMPORT_TEMPLATE="${URL} ${IMPORT}"
 
-echo "CLEANING UP"
-find . -iname "*.clean" -exec echo {} \+ -exec rm -v {} \;
+cleanup() {
+    echo "CLEANING UP"
+    find . -iname "*.clean" -exec echo {} \+ -exec rm -v {} \;
+}
 
 echo "UPDATE IMPORTS(${IMPORT}) to " ${IMPORT_TEMPLATE}
 # exit 0 #-- 
 
+cleanup
+
 find . -iname "*.hs*" -exec echo {} \; \
   -exec grep -i -n -e "import ${IMPORT}" {} \+ \
   -exec sed -i.clean -E -e "s/.*import ${IMPORT}.*/import ${IMPORT} -- ${IMPORT_TEMPLATE//\//\\/}/" {} \;
+
+
+
